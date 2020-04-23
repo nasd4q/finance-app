d5296a4905abef9405683c31d16d4d7fa7ec034d
nasd4q <nasd4q@gmail.com>
2020-04-23 22:26:40 +0200

    financeDB module freshly created and imported from Spring Initializr

---
+++ financeDB/src/main/java/com/nasd4q/financeDB/FinanceDbApplication.java
@@ -0,0 +1,13 @@
+package com.nasd4q.financeDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceDbApplication.class, args);
+	}
+
+}
---
+++ financeDB/src/test/java/com/nasd4q/financeDB/FinanceDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nasd4q.financeDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
